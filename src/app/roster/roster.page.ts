942fbe1e520e92edff21f11123005612a4d6a54a
Mikael Trilles <11914687-mkaeltrilles@users.noreply.gitlab.com>
2022-07-04 17:36:11 +0200

    👌 IMPROVE: present-absent

--- src/app/roster/roster.page.ts
+++ src/app/roster/roster.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Student, StudentService} from "../student.service";
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-roster',
@@ -11,10 +12,47 @@ export class RosterPage implements OnInit {
   students:Student[] = [];
   constructor(
     private studentService : StudentService,
+    private actionSheetController: ActionSheetController,
   ) { }
+
 
   ngOnInit() {
     this.students = this.studentService.getAll();
   }
+
+  async presentActionSheet(student: Student) {
+    const actionSheet = await this.actionSheetController.create({
+      header: `${student.firstName} ${student.lastName}`,
+      buttons: [{
+        text: 'Présent',
+        icon: 'eye',
+        handler: () => {
+          student.status = 'present';
+        }
+      },{
+        text: 'Absent',
+        icon: 'eye-off-outline',
+        handler: () => {
+          student.status= 'absent';
+        }
+      },{
+        text: 'Supprimer',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Supprimer');
+        }
+      },   {
+        text: 'Annuler',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Annuler');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 
 }
