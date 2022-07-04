63fd2127ce950a14736861762248ff5516240571
Mikael Trilles <11914687-mkaeltrilles@users.noreply.gitlab.com>
2022-07-04 16:53:38 +0200

    first commit

---
+++ src/app/roster/roster.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Student, StudentService} from "../student.service";
+
+@Component({
+  selector: 'app-roster',
+  templateUrl: './roster.page.html',
+  styleUrls: ['./roster.page.scss'],
+})
+
+export class RosterPage implements OnInit {
+  students:Student[] = [];
+  constructor(
+    private studentService : StudentService,
+  ) { }
+
+  ngOnInit() {
+    this.students = this.studentService.getAll();
+  }
+
+}
---
+++ src/app/student.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+export interface Student {
+  id: string;
+  firstName: string;
+  lastName: string;
+  birthDate?: string;
+  parentName?: string;
+  parentEmail?: string;
+  parentPhone?: string;
+  photoUrl?: string;
+  status?: 'present' | 'absent';
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+  export class StudentService {
+  mockStudents: Student[] = [
+    {id: '1', firstName: 'Jean', lastName: 'Bon', status: 'present'},
+    {id: '2', firstName: 'Pierre', lastName: 'Afeu'},
+    {id: '3', firstName: 'Harry', lastName: 'Covert',status: 'absent'},
+    {id: '4', firstName: 'Corinne', lastName: 'Tigoute'},
+    {id: '5', firstName: 'Mélisine', lastName: 'Enfaillite'}
+  ]
+
+  constructor() { }
+  getAll(){
+    return this.mockStudents;
+  }
+
+}
+
+
+
