c52dc971f3281f318627598dd9bcc93cc61cff14
Mikael Trilles <11914687-mkaeltrilles@users.noreply.gitlab.com>
2022-07-05 12:39:49 +0200

    👌 IMPROVE: Ajout Menu burger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+
+export interface Page {
+  title: string;
+  url: string,
+  icon: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public selectedIndex = 0;
+  public appPages: Page[] = [
+    {
+      title: 'Accueil',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Etudiants',
+      url: '/roster',
+      icon: 'people'
+    },
+  ];
   constructor() {}
 }
